610e23c0eff00e921a6f681490c6ca351e3b170a
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-08-25 20:27:05 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
+import banana from "../src/assets/banana.png";
+import { motion } from "framer-motion";
+import { useState } from "react";
+
 function App() {
+  const [click, setClick] = useState(false);
+
+  const generateX = () => {
+    let num = Math.floor(Math.random() * 350);
+    if (num < 20) {
+      generateX();
+    } else {
+      console.log(num);
+      return num;
+    }
+  };
+
   return (
     <div>
-      <div></div>
+      <div className=" absolute">
+        <button
+          className="bg-slate-500"
+          onClick={() => {
+            setClick(true);
+          }}
+        >
+          banana drop
+        </button>
+        <motion.div
+          animate={
+            click
+              ? {
+                  rotate: [0, 180],
+                  y: [-100, 800],
+                  opacity: [1, 1, 0],
+                  x: [generateX(), generateX()],
+                }
+              : {}
+          }
+          transition={{ duration: 2, ease: "linear" }}
+          className={click ? null : "hidden"}
+        >
+          <img src={banana} alt="banana" className="w-8" />
+        </motion.div>
+      </div>
     </div>
   );
 }
