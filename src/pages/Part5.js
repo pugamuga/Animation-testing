8a94bcaea81b937a15f0fecd7c6a7b2732d8c185
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-09-15 10:18:58 +0300

    start part5

--- src/pages/Part5.js
+++ src/pages/Part5.js
@@ -3,6 +3,20 @@ import warImage from "../assets/war.jpg";
 
 
 const Part5 = ({setPageNum}) => {
+
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    fetchPopular()
+  }, []);
+
+  const fetchPopular = async()=>{
+    const data = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=e74a6d3be393855d28d9897760e76b51&language=en-US&page=1')
+    const movies = await data.json()
+    setPopular(movies.results)
+ console.log(movies)
+  }
+
   useEffect(() => {
     setPageNum(5)
   }, []);
